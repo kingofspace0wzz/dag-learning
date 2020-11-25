b4eb47d191f691fedf4c62684333ff1f9e22e3e6
kingofspace0wzz <zizhuang_wang@umail.ucsb.edu>
2020-11-25 02:23:44 -0800

    update

---
+++ flow-dag/train_baseline_repeat.sh
@@ -0,0 +1,4 @@
+for i in {1..10}
+do 
+    ./train_baseline_step.sh
+done
---
+++ flow-dag/train_baseline_step.sh
@@ -0,0 +1,33 @@
+graph_type=('erdos-renyi' 'barabasi-albert')
+graph_linear_type=('linear' 'nonlinear_1' 'nonlinear_2')
+graph_sem_type=('linear-gauss' 'linear-exp' 'linear-exp')
+graph_degree=(3 4)
+data_variable_size=(10 20 50 100)
+
+# for gt in "${graph_type[@]}"
+# do
+#     for glt in "${graph_linear_type[@]}"
+#     do
+#         for gst in "${graph_sem_type[@]}"
+#         do
+#             for j in 3 4
+#             do
+#                 python3.6 baseline.py --graph_type "$gt" --graph_linear_type "$glt" --graph_sem_type "$gst" --graph_degree $j --data_variable_size 10 &
+#                 python3.6 baseline.py --graph_type "$gt" --graph_linear_type "$glt" --graph_sem_type "$gst" --graph_degree $j --data_variable_size 20 &
+#                 python3.6 baseline.py --graph_type "$gt" --graph_linear_type "$glt" --graph_sem_type "$gst" --graph_degree $j --data_variable_size 50 &
+#                 python3.6 baseline.py --graph_type "$gt" --graph_linear_type "$glt" --graph_sem_type "$gst" --graph_degree $j --data_variable_size 100 & 
+#             done
+#         done
+#     done
+# done
+
+for i in 3 4
+do
+    for gst in "${graph_sem_type[@]}"
+    do
+        for j in 10 20 50 100
+        do
+            python3.6 baseline.py --graph_type erdos-renyi --graph_linear_type nonlinear_1 --graph_sem_type "$gst" --graph_degree $i --data_variable_size $j &
+        done
+    done
+done
